fa1caec48476bbf3a31b9ace08ed14a36d416a4c
Vincent Chu <c.tnecniv@gmail.com>
2013-08-13 00:30:03 -0700

    ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Membr::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  # config.force_ssl = true
+  config.force_ssl = true
 end
